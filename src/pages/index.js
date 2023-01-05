037335780e99c4fc4c8227969508db075601db77
NiuBoss123 <i@niuboss123.com>
2023-01-06 04:59:46 +0800

    2023-001-06-02

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            现在下载 
+            to="/intro">
+            现在开始 
           </Link>
         </div>
       </div>
